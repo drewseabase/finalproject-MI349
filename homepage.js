a9b5a4da222b7dedcda0e4199fa449fb3f3677e8
drewseabase <143631824+drewseabase@users.noreply.github.com>
2025-11-19 16:59:50 -0500

    adding javascript files

---
+++ homepage.js
@@ -0,0 +1,39 @@
+ document.addEventListener("DOMContentLoaded", function () {
+        const rosterImages = [
+        "Images/rileyhough.PNG",
+        "Images/thomaswestphal.PNG",
+        "Images/jackpennewell.PNG",
+        "Images/mosesosterink.PNG",
+        "Images/mikehegarty.PNG",
+        "Images/parkerlambers.PNG",
+        "Images/jaspercane.PNG",
+        "Images/paddybyrnes.PNG",
+        "Images/evanloughridge.PNG",
+        "Images/bradymcardle.PNG",
+        "Images/drewseabase.PNG",
+        "Images/nathanlarson.PNG"
+        ];
+
+        const rosterEl = document.querySelector(".roster-slideshow");
+        if (!rosterEl || rosterImages.length === 0) return;
+
+        let currentIndex = 0;
+
+        rosterEl.src = rosterImages[0];
+
+        function showNextRosterImage() {
+
+        rosterEl.classList.add("is-fading");
+
+        setTimeout(() => {
+            currentIndex = (currentIndex + 1) % rosterImages.length;
+            rosterEl.src = rosterImages[currentIndex];
+        }, 1000);
+
+        setTimeout(() => {
+            rosterEl.classList.remove("is-fading");
+        }, 1100);
+        }
+
+        setInterval(showNextRosterImage, 10000);
+    });
