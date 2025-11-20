2f3367baa843d551b0a19a85912bf8585ef69d77
drewseabase <143631824+drewseabase@users.noreply.github.com>
2025-11-20 11:23:56 -0500

    i think i fixed it for sure

--- homepage.js
+++ homepage.js
@@ -1,18 +1,18 @@
  document.addEventListener("DOMContentLoaded", function () {
-       // const rosterImages = [
-       // "Images/rileyhough.PNG",
-       // "Images/thomaswestphal.PNG",
-       // "Images/jackpennewell.PNG",
-       // "Images/mosesosterink.PNG",
-       // "Images/mikehegarty.PNG",
-       // "Images/parkerlambers.PNG",
-       // "Images/jaspercane.PNG",
-       // "Images/paddybyrnes.PNG",
-      //  "Images/evanloughridge.PNG",
-       // "Images/bradymcardle.PNG",
-       // "Images/drewseabase.PNG",
-       // "Images/nathanlarson.PNG"
-      //  ];
+        const rosterImages = [
+        "Images/rileyhough.PNG",
+        "Images/thomaswestphal.PNG",
+        "Images/jackpennewell.PNG",
+        "Images/mosesosterink.PNG",
+        "Images/mikehegarty.PNG",
+        "Images/parkerlambers.PNG",
+        "Images/jaspercane.PNG",
+        "Images/paddybyrnes.PNG",
+        "Images/evanloughridge.PNG",
+        "Images/bradymcardle.PNG",
+        "Images/drewseabase.PNG",
+        "Images/nathanlarson.PNG"
+        ];
 
         const rosterEl = document.querySelector(".roster-slideshow");
         if (!rosterEl || rosterImages.length === 0) return;
