8d2cbe5dbf8aadcc8d931dce7f4d124b0d70a7ba
drewseabase <143631824+drewseabase@users.noreply.github.com>
2025-11-26 08:34:33 -0500

    longer transition

--- homepage.js
+++ homepage.js
@@ -29,7 +29,7 @@
             currentIndex = (currentIndex + 1) % rosterImages.length;
             rosterEl.src = rosterImages[currentIndex];
             rosterEl.classList.remove("is-fading");
-        }, 1000);
+        }, 2000);
         }
 
         setInterval(showNextRosterImage, 6000);
