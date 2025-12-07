9c68a4facca892127409d138011cdf8e64654b18
drewseabase <143631824+drewseabase@users.noreply.github.com>
2025-12-07 17:29:14 -0500

    see if this works

--- homepage.js
+++ homepage.js
@@ -1,36 +1,42 @@
- document.addEventListener("DOMContentLoaded", function () {
-        const rosterImages = [
-        "Images/rileyhough.PNG",
-        "Images/thomaswestphal.PNG",
-        "Images/jackpennewell.PNG",
-        "Images/mosesosterink.PNG",
-        "Images/mikehegarty.PNG",
-        "Images/parkerlambers.PNG",
-        "Images/jaspercane.PNG",
-        "Images/paddybyrnes.PNG",
-        "Images/evanloughridge.PNG",
-        "Images/bradymcardle.PNG",
-        "Images/drewseabase.PNG",
-        "Images/nathanlarson.PNG"
-        ];
+document.addEventListener("DOMContentLoaded", function () {
+  const rosterImages = [
+    "Images/rileyhough.PNG",
+    "Images/thomaswestphal.PNG",
+    "Images/jackpennewell.PNG",
+    "Images/mosesosterink.PNG",
+    "Images/mikehegarty.PNG",
+    "Images/parkerlambers.PNG",
+    "Images/jaspercane.PNG",
+    "Images/paddybyrnes.PNG",
+    "Images/evanloughridge.PNG",
+    "Images/bradymcardle.PNG",
+    "Images/drewseabase.PNG",
+    "Images/nathanlarson.PNG"
+  ];
 
-        const rosterEl = document.querySelector(".roster-slideshow");
-        if (!rosterEl || rosterImages.length === 0) return;
+  const rosterEl = document.querySelector(".roster-slideshow");
+  if (!rosterEl || rosterImages.length === 0) return;
 
-        let currentIndex = 0;
+  let currentIndex = 0;
+  rosterEl.src = rosterImages[0];
 
-        rosterEl.src = rosterImages[0];
+  function showNextRosterImage() {
+    rosterEl.classList.add("is-fading");
 
-        function showNextRosterImage() {
+    const onEnd = (e) => {
+      if (e.propertyName !== "opacity") return;
+      rosterEl.removeEventListener("transitionend", onEnd);
 
-        rosterEl.classList.add("is-fading");
+      currentIndex = (currentIndex + 1) % rosterImages.length;
+      rosterEl.src = rosterImages[currentIndex];
 
-        setTimeout(() => {
-            currentIndex = (currentIndex + 1) % rosterImages.length;
-            rosterEl.src = rosterImages[currentIndex];
-            rosterEl.classList.remove("is-fading");
-        }, 600);
-        }
+      requestAnimationFrame(() => {
+        rosterEl.classList.remove("is-fading");
+      });
+    };
 
-        setInterval(showNextRosterImage, 7000);
-    });
+    rosterEl.addEventListener("transitionend", onEnd);
+  }
+
+  setInterval(showNextRosterImage, 7000);
+});
