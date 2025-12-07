54135c46d276dbf4aefc3feaf12f7d80fd05e914
drewseabase <143631824+drewseabase@users.noreply.github.com>
2025-12-07 17:30:47 -0500

    plz

--- homepage.js
+++ homepage.js
@@ -1,42 +1,36 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const rosterImages = [
-    "Images/rileyhough.PNG",
-    "Images/thomaswestphal.PNG",
-    "Images/jackpennewell.PNG",
-    "Images/mosesosterink.PNG",
-    "Images/mikehegarty.PNG",
-    "Images/parkerlambers.PNG",
-    "Images/jaspercane.PNG",
-    "Images/paddybyrnes.PNG",
-    "Images/evanloughridge.PNG",
-    "Images/bradymcardle.PNG",
-    "Images/drewseabase.PNG",
-    "Images/nathanlarson.PNG"
-  ];
+ document.addEventListener("DOMContentLoaded", function () {
+        const rosterImages = [
+        "Images/rileyhough.PNG",
+        "Images/thomaswestphal.PNG",
+        "Images/jackpennewell.PNG",
+        "Images/mosesosterink.PNG",
+        "Images/mikehegarty.PNG",
+        "Images/parkerlambers.PNG",
+        "Images/jaspercane.PNG",
+        "Images/paddybyrnes.PNG",
+        "Images/evanloughridge.PNG",
+        "Images/bradymcardle.PNG",
+        "Images/drewseabase.PNG",
+        "Images/nathanlarson.PNG"
+        ];
 
-  const rosterEl = document.querySelector(".roster-slideshow");
-  if (!rosterEl || rosterImages.length === 0) return;
+        const rosterEl = document.querySelector(".roster-slideshow");
+        if (!rosterEl || rosterImages.length === 0) return;
 
-  let currentIndex = 0;
-  rosterEl.src = rosterImages[0];
+        let currentIndex = 0;
 
-  function showNextRosterImage() {
-    rosterEl.classList.add("is-fading");
+        rosterEl.src = rosterImages[0];
 
-    const onEnd = (e) => {
-      if (e.propertyName !== "opacity") return;
-      rosterEl.removeEventListener("transitionend", onEnd);
+        function showNextRosterImage() {
 
-      currentIndex = (currentIndex + 1) % rosterImages.length;
-      rosterEl.src = rosterImages[currentIndex];
+        rosterEl.classList.add("is-fading");
 
-      requestAnimationFrame(() => {
-        rosterEl.classList.remove("is-fading");
-      });
-    };
+        setTimeout(() => {
+            currentIndex = (currentIndex + 1) % rosterImages.length;
+            rosterEl.src = rosterImages[currentIndex];
+            rosterEl.classList.remove("is-fading");
+        }, 800);
+        }
 
-    rosterEl.addEventListener("transitionend", onEnd);
-  }
-
-  setInterval(showNextRosterImage, 7000);
-});
+        setInterval(showNextRosterImage, 7000);
+    });
