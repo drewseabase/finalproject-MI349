34a984506c5aef1e0fb69b7d3d6b4f4f9612f2f6
drewseabase <143631824+drewseabase@users.noreply.github.com>
2026-01-08 11:22:00 -0500

    okie

--- schedule.js
+++ schedule.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   
+  //Finds all Elements with correlating class to avoid repeating DOM queries
   const items = document.querySelectorAll('.meets-list .item');
-
+  //Checks if browser supports API
   if ('IntersectionObserver' in window) {
     const observer = new IntersectionObserver(
       (entries) => {
@@ -17,26 +18,28 @@ document.addEventListener('DOMContentLoaded', () => {
         threshold: 0.55, 
       }
     );
-
+    //Tells window to watch each .item element
     items.forEach((item) => observer.observe(item));
   } else {
-    
+    //Fall back incase intersection observer isn't supported
     items.forEach((item) => item.classList.add('is-visible'));
   }
 
-
+  //Code for Modal Responsiveness
   const modal = document.querySelector('.course-modal');
+  //If modals exist find content, if not, null
   const modalBody = modal ? modal.querySelector('.course-modal-body') : null;
   const closeBtn = modal ? modal.querySelector('.course-modal-close') : null;
   const backdrop = modal ? modal.querySelector('.course-modal-backdrop') : null;
 
+  //Opens Modal
   function openModal(html) {
     if (!modal || !modalBody) return;
     modalBody.innerHTML = html;
     modal.classList.add('is-open');
-    document.body.style.overflow = 'hidden';
+    document.body.style.overflow = 'hidden'; //prevents page from scrolling
   }
-
+  //Closes Modal
   function closeModal() {
     if (!modal) return;
     modal.classList.remove('is-open');
